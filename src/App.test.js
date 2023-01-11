07a71350ccaa606ae0030a8cb4bb5b8447719c6f
unknown <kashif_saleem_23@hotmail.com>
2023-01-11 08:53:49 +0500

    add change for linter correction

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import MathApp from './MathApp';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<MathApp />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
