21a6f43ccbc10ef5edbce1fe278054a9cd1fb614
unknown <kashif_saleem_23@hotmail.com>
2023-01-12 13:35:50 +0500

    refactor to use function and hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,57 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { total: 0, next: null, operation: null };
-  }
-
-  gotClicked = (e) => {
+const Calculator = () => {
+  const [state, setState] = useState({ total: 0, next: null, operation: null });
+  const { total, next, operation } = state;
+  const gotClicked = (e) => {
     const clickedButton = e.target.innerText;
-    const gotValue = calculate(this.state, clickedButton);
-    this.setState(gotValue);
-  }
+    const gotValue = calculate(state, clickedButton);
+    setState(gotValue);
+  };
+  return (
+    <>
+      <article className="container">
+        <section className="result">
+          <span>
+            {total}
+            {' '}
+            {operation}
+            {' '}
+            {next}
+          </span>
+        </section>
+        <section className="wrapper">
+          <button type="button" onClick={gotClicked}>AC</button>
+          <button type="button" onClick={gotClicked}>+/-</button>
+          <button type="button" onClick={gotClicked}>%</button>
+          <button type="button" className="operators" onClick={gotClicked}>{'\u00F7'}</button>
+          <button type="button" onClick={gotClicked}>7</button>
+          <button type="button" onClick={gotClicked}>8</button>
+          <button type="button" onClick={gotClicked}>9</button>
+          <button type="button" className="operators" onClick={gotClicked}>x</button>
+          <button type="button" onClick={gotClicked}>4</button>
+          <button type="button" onClick={gotClicked}>5</button>
+          <button type="button" onClick={gotClicked}>6</button>
+          <button type="button" className="operators" onClick={gotClicked}>-</button>
+          <button type="button" onClick={gotClicked}>1</button>
+          <button type="button" onClick={gotClicked}>2</button>
+          <button type="button" onClick={gotClicked}>3</button>
+          <button type="button" className="operators" onClick={gotClicked}>+</button>
+          <button type="button" className="zero" onClick={gotClicked}>0</button>
+          <button type="button" className="dot" onClick={gotClicked}>.</button>
+          <button type="button" className="operators" onClick={gotClicked}>=</button>
+        </section>
+      </article>
 
-  render() {
-    const { total, operation, next } = this.state;
-    return (
-      <>
-        <article className="container">
-          <section className="result">
-            <span>
-              {total}
-              {operation}
-              {next}
-            </span>
-          </section>
-          <section className="wrapper">
-            <button type="button" onClick={this.gotClicked}>AC</button>
-            <button type="button" onClick={this.gotClicked}>+/-</button>
-            <button type="button" onClick={this.gotClicked}>%</button>
-            <button type="button" className="operators" onClick={this.gotClicked}>{'\u00F7'}</button>
-            <button type="button" onClick={this.gotClicked}>7</button>
-            <button type="button" onClick={this.gotClicked}>8</button>
-            <button type="button" onClick={this.gotClicked}>9</button>
-            <button type="button" className="operators" onClick={this.gotClicked}>x</button>
-            <button type="button" onClick={this.gotClicked}>4</button>
-            <button type="button" onClick={this.gotClicked}>5</button>
-            <button type="button" onClick={this.gotClicked}>6</button>
-            <button type="button" className="operators" onClick={this.gotClicked}>-</button>
-            <button type="button" onClick={this.gotClicked}>1</button>
-            <button type="button" onClick={this.gotClicked}>2</button>
-            <button type="button" onClick={this.gotClicked}>3</button>
-            <button type="button" className="operators" onClick={this.gotClicked}>+</button>
-            <button type="button" className="zero" onClick={this.gotClicked}>0</button>
-            <button type="button" className="dot" onClick={this.gotClicked}>.</button>
-            <button type="button" className="operators" onClick={this.gotClicked}>=</button>
-          </section>
-        </article>
+    </>
 
-      </>
-
-    );
-  }
-}
+  );
+};
 
 export default Calculator;
