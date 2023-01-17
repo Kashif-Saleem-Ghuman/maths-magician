145a47a13382b1df70a0f9210df85257e4f48557
unknown <kashif_saleem_23@hotmail.com>
2023-01-17 22:59:23 +0500

    add routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Home from './components/Home';
+import Quotes from './components/Quotes';
 
 export default function App() {
   return (
-    <div><Calculator /></div>
+    <>
+      <nav className="Nav-bar">
+        <ul>
+          <li>
+            <Link className="Link" to="/home">Home</Link>
+          </li>
+          <li>
+            <Link className="Link" to="/calculator">Calculator</Link>
+          </li>
+          <li>
+            <Link className="Link" to="/quotes">Quotes</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quotes" element={<Quotes />} />
+      </Routes>
+    </>
+
   );
 }
---
+++ src/components/Quotes.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Quotes() {
+  return (
+    <div>Quotes</div>
+  );
+}
