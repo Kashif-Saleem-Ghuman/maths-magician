2d41cdf76b03bdb90b6e9eb8d4bcd4b974ea4f36
unknown <kashif_saleem_23@hotmail.com>
2023-01-19 15:55:12 +0500

    calculate test performed

--- src/logic/__test__/calculate.test.js
+++ src/logic/__test__/calculate.test.js
@@ -14,4 +14,47 @@ describe('testing each button', () => {
     const object = { ...obj, next: '0' };
     expect(calculate(object, '0')).toEqual({});
   });
+  test('should update next if there is an operation, ', () => {
+    const object = { ...obj, next: '1', operation: '+' };
+    expect(calculate(object, '2')).toEqual({ ...object, next: `${object.next}2` });
+  });
+  test('if buttonName is . ', () => {
+    const object = { ...obj, next: '0.2' };
+    expect(calculate(object, '.')).toEqual({ ...object });
+  });
+  test('if operation is = ', () => {
+    const object = {
+      ...obj, next: '2', total: '5', operation: '+',
+    };
+    expect(calculate(object, '=')).toEqual({
+      ...object, total: '7', next: null, operation: null,
+    });
+  });
+  test('should do nothing if there is no operation or no next is = ', () => {
+    const object = {
+      ...obj, next: '2', total: '5', operation: null,
+    };
+    expect(calculate(object, '=')).toEqual({});
+  });
+  test(" should return the object followed by '+'", () => {
+    const object = { ...obj, total: '5' };
+    expect(calculate(object, '+')).toEqual({ ...object, operation: '+' });
+  });
+  test('if obj.total and obj.next is false', () => {
+    const object = { ...obj, total: '50', operation: '-' };
+    expect(calculate(object, '+')).toEqual({ ...object, operation: '+' });
+  });
+  test('if total and next is false', () => {
+    const object = { ...obj, total: '0', operation: '-' };
+    expect(calculate(object, 'x')).toEqual({ ...object, total: '0', operation: 'x' });
+  });
+
+  test('Testing if obj.total is false', () => {
+    const object = { ...obj, total: '0', operation: '+' };
+    expect(calculate(object, '+')).toEqual({ ...object });
+  });
+  test("The user hasn't typed a number yet", () => {
+    const object = { ...obj };
+    expect(calculate(object, '+')).toEqual({ operation: '+' });
+  });
 });
